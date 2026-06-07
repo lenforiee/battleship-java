c0a91033d67c1ae4e58af5c920987f65f9443ca1
Marcelin <lenforiee@gmail.com>
2026-06-07 10:53:39 +0200

    chore: move usernames to game config and add user boards

--- src/config/GameConfig.java
+++ src/config/GameConfig.java
@@ -1,6 +1,12 @@
 package config;
 
+import components.BattleBoard;
+
 public class GameConfig {
     public static int matrixSize = 10;
     public static boolean bonusMoveOnHit = true;
+    public static String username1 = "";
+    public static String username2 = "";
+    public static BattleBoard userBoard1 = new BattleBoard();
+    public static BattleBoard userBoard2 = new BattleBoard();
 }
